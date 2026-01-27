de654f04ea6f19e9e02fc1c5a0ad809ac977be2f
Darkus17 <tulaganovaleisan@gmail.com>
2026-01-27 09:27:33 +0500

    Rewrite main page test using Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,8 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-
-    login_link = WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable((By.ID, "login_link"))
-    )
-    login_link.click()
+    page = MainPage(browser, link)   # создаём Page Object
+    page.open()                      # открываем главную страницу
+    page.go_to_login_page()          # переходим на страницу логина
